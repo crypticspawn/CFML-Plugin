cf144e3c605352fc4f38e031ed0d0884a6f9b9e8
GitHub Action <action@github.com>
2022-01-26 03:54:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/crypticspawn/cfmlplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/crypticspawn/cfmlplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.crypticspawn.cfmlplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crypticspawn.cfmlplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/crypticspawn/cfmlplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/crypticspawn/cfmlplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.crypticspawn.cfmlplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crypticspawn.cfmlplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
